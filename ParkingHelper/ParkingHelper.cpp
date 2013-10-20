9c40dcf39df72b0ccf8a3c40af1f6ec0d88bff2f
matthew-humphrey <mhumphrey@gmail.com>
2013-10-19 17:56:09 -0700

    Got basic distance measurement and display working again.

--- ParkingHelper/ParkingHelper.cpp
+++ ParkingHelper/ParkingHelper.cpp
@@ -14,9 +14,10 @@
 
 const uint8_t NUM_LEDS = 4;
 const uint32_t TIMEOUT_CYCLES = F_CPU / 1000 * 20;
-const uint8_t MICROSECONDS_PER_TICK = 8;
-const uint16_t cautionDistanceCm = 150;
+const uint8_t USECS_PER_FAST_INT = 10;
+const uint8_t MSECS_PER_SLOW_INT = 1;
 
+volatile uint32_t ticks = 0;
 volatile uint16_t echoTicks = 0;
 
 LPD8806 leds(NUM_LEDS, PB0 /* data */, PB2 /* clock */);
@@ -29,7 +30,7 @@ void setAllLedsToColor(const Color& color)
 	}
 }
 
-uint16_t readDistance()
+float readDistanceCm()
 {
 	uint32_t timeout;
 	
@@ -66,70 +67,63 @@ uint16_t readDistance()
 		}
 	}
 		
-	uint16_t ticks;
+	uint16_t duration;
 	ATOMIC_BLOCK(ATOMIC_FORCEON)
 	{
-		ticks = echoTicks;		
+		duration = echoTicks;		
 	}
 	
-	/*
-	Color c0 = Color( ((ticks >> 11) & 0x1) * 127, ((ticks >> 10) & 0x1) * 127, ((ticks >> 9) & 0x1) * 127);
-	Color c1 = Color( ((ticks >> 8) & 0x1) * 127, ((ticks >> 7) & 0x1) * 127, ((ticks >> 6) & 0x1) * 127);
-	Color c2 = Color( ((ticks >> 5) & 0x1) * 127, ((ticks >> 4) & 0x1) * 127, ((ticks >> 3) & 0x1) * 127);
-	Color c3 = Color( ((ticks >> 2) & 0x1) * 127, ((ticks >> 1) & 0x1) * 127, ((ticks >> 0) & 0x1) * 127);
-	leds.setPixelColor(0, c0);
-	leds.setPixelColor(1, c1);
-	leds.setPixelColor(2, c2);
-	leds.setPixelColor(3, c3);
-	leds.show();
-	*/
-	
-	// return ticks * ((float)MICROSECONDS_PER_TICK * 34029.0f / 1000000.0f);
-	return ticks;
+	return (float)duration * (float)USECS_PER_FAST_INT * (34029.0f / 2.0f / 1000000.0f);
 }
 
 int main(void)
 {
-	uint16_t distance;
-
 	leds.begin();
 	setAllLedsToColor(Color::Black);
 	leds.show();
 	
 	PORTB |= (1 << PINB3); // Enable pull-up resistor on PB3 (switch)
 	
-	TCCR0B |= (1 << CS00);	// Clk / 1
-	OCR0A = (F_CPU / 1000000UL) * MICROSECONDS_PER_TICK - 1;
-	TCCR0B |= WGM01;	// CTC timer mode
+	// Configure Timer0 for 10 us interrupts
+	TCCR0A |= _BV(WGM01);	// CTC timer mode
+	TCCR0B |= _BV(CS00);	// Pre-scaler -> CPU clock / 1
+	OCR0A = (F_CPU / 1000000UL) * USECS_PER_FAST_INT - 1;
 	TCNT0 = 0;	// Clear counter
-	TIMSK |= (1 << OCIE0A);	// Enable output compare match interrupt
+	TIMSK |= _BV(OCIE0A);	// Enable output compare match interrupt
 	
-	sei();
+	// Configure Timer1 for 1 ms interrupts
+	TCCR1 |= _BV(CTC1) | _BV(CS13);	// CTC mode, pre-scaler -> CPU clock / 128
+	OCR1C = (F_CPU / 128UL / 1000UL) * MSECS_PER_SLOW_INT - 1; // Clear counter at 125
+	OCR1A = OCR1C;	// On the ATTiny, the OCR1A or OCR1B must be used to generate the actual interrupt
+	TIMSK |= _BV(OCIE1A); // Enable output match compare interrupt
+	
+	sei();	// Enable global interrupts
 			
-	// TODO: Allow user to set minimum distance value
-	uint16_t stopDistanceCm = 15;
+	float distance;
+	const float stopDistance = 15.0f;
+	const float cautionDistance = 150.0f;
 	
 	while(1)
 	{
-		distance = readDistance();	// Distance is in ticks. When MICROSECONDS_PER_TICK = 32, each tick is about a centimeter.
+		distance = readDistanceCm();
 		if (distance == 0)
 		{
 			// No reading / timeout
 			setAllLedsToColor(Color::Black);
 		}
-		else if (distance < stopDistanceCm * 32 / MICROSECONDS_PER_TICK)
+		else if (distance < stopDistance)
 		{
 			setAllLedsToColor(Color::Red);
 		}
-		else if (distance > cautionDistanceCm * 32 / MICROSECONDS_PER_TICK)
+		else if (distance > cautionDistance)
 		{
 			setAllLedsToColor(Color::Lime);
 		}
 		else
 		{
-			uint16_t maxd = cautionDistanceCm * 32 / MICROSECONDS_PER_TICK;
-			uint16_t mind = stopDistanceCm * 32 / MICROSECONDS_PER_TICK;
-			uint16_t interval = (maxd - mind) / leds.numPixels();
+			float maxd = cautionDistance;
+			float mind = stopDistance;
+			float interval = (maxd - mind) / leds.numPixels();
 			
 			mind += interval;
 			for (int i = leds.numPixels()-1; i >= 0; --i)
@@ -144,7 +138,6 @@ int main(void)
 				}
 				mind += interval;
 			}
-			// setAllLedsToColor(Color::Yellow);
 		}
 		leds.show();
 		
@@ -163,4 +156,27 @@ ISR(TIMER0_COMPA_vect)
 	{
 		++echoTicks;
 	}
+}
+
+// The ISR_NOBLOCK allows nested interrupts so that the hi-res timer (Timer0) handler will not be blocked by this low-res timer code. 
+// This improves accuracy of the distance measurements. We specifically disable the Timer1 interrupt during this routine so that we
+// do not have nested Timer1 interrupts (and the potential for stack overflow).
+ISR(TIMER1_COMPA_vect, ISR_NOBLOCK)
+{
+	// Disable this interrupt because we do not want to allow THIS interrupt to nest, potentially leading to a stack overflow situation
+	TIMSK &= ~_BV(OCIE1A);
+	
+	++ticks;	
+
+	volatile static uint8_t tenTicks = 0;
+	if (++tenTicks >= 10)
+	{
+		// TODO: Call LED sequencer
+		tenTicks = 0;
+	}
+		
+	// Safely re-enable this interrupt
+	cli();
+	TIMSK |= _BV(OCIE1A);
+	// Global interrupts will be enabled upon execution of the RETI instruction
 }
